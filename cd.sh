ee89018ef81fb73c69209acc5cd9b613b2260f63
AswiniVayyala <vayyala.aswini@gmail.com>
2023-05-28 10:44:43 +1000

    add ci and cd commands

--- cd.sh
+++ cd.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # This file is used to simulating the CD process
+cd ../terraform_iac
+terraform init
+terraform validate
+terraform apply
+#Upload build content to S3
+aws s3 sync ../test-app/build s3://<s3_bucket_name>/
